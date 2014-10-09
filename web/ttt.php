e5aafcca439ba380a63ae7fd876ac0c10c7bb5d0
Polish <polishwb1020@gmail.com>
2014-10-10 01:44:55 +0800

    work on the new.php ///use 'service mysqld restart' to restart the mysql

--- web/ttt.php
+++ web/ttt.php
@@ -4,6 +4,9 @@
 <TITLE></TITLE>
 </HEAD>
 <BODY topmargin="0" bottomMargin=0 rightmargin=0 leftmargin=0>
-<img src="./img/logo.gif" style="width:100%">
+<img src="./image/logo.gif" style="width:100%">
+<?php
+	include_once('include.inc');
+?>
 </BODY>
 </HTML>
